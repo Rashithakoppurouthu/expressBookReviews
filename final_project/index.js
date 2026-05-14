549a0a2fc612d423f328e77c830b5fcd56056ff9
Rashitha Koppurouthu <rashithakoppurouthu@gmail.com>
2026-05-14 12:16:17 +0530

    final axios update

--- final_project/index.js
+++ final_project/index.js
@@ -6,9 +6,9 @@ const customer_routes = require('./router/auth_users.js').authenticated;
 const genl_routes = require('./router/general.js').general;
 
 const app = express();
-
 app.use(express.json());
 
+// session middleware
 app.use(
     "/customer",
     session({
@@ -18,30 +18,45 @@ app.use(
     })
 );
 
+// auth middleware (required for assignment structure)
 app.use("/customer/auth/*", function auth(req, res, next) {
-    next();
+    let token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(403).json({ message: "Login required" });
+    }
+
+    try {
+        token = token.split(" ")[1];
+        const user = jwt.verify(token, "fingerprint_customer");
+        req.user = user;
+        next();
+    } catch (err) {
+        return res.status(401).json({ message: "Invalid Token" });
+    }
 });
 
-const PORT = 5000;
-
+// routes
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
+// register users
 const users = [];
 
 app.post("/register", (req, res) => {
+    const { username, password } = req.body;
 
-    const username = req.body.username;
-    const password = req.body.password;
+    const exists = users.find(u => u.username === username);
+    if (exists) {
+        return res.status(400).json({ message: "User already exists" });
+    }
 
-    users.push({
-        username,
-        password
-    });
+    users.push({ username, password });
 
     return res.status(200).json({
         message: "User successfully registered. Now you can login"
     });
 });
 
-app.listen(PORT, () => console.log("Server is running"));
+const PORT = 5000;
+app.listen(PORT, () => console.log("Server is running"))
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -2,21 +2,16 @@ const express = require('express');
 const public_users = express.Router();
 
 let books = {
-    "1": {
-        "author": "Chinua Achebe",
-        "title": "Things Fall Apart",
-        "reviews": {}
-    },
-    "2": {
-        "author": "Hans Christian Andersen",
-        "title": "Fairy tales",
-        "reviews": {}
-    },
-    "3": {
-        "author": "Dante Alighieri",
-        "title": "The Divine Comedy",
-        "reviews": {}
-    }
+    "1": { "author": "Chinua Achebe", "title": "Things Fall Apart", "reviews": {} },
+    "2": { "author": "Hans Christian Andersen", "title": "Fairy tales", "reviews": {} },
+    "3": { "author": "Dante Alighieri", "title": "The Divine Comedy", "reviews": {} },
+    "4": { "author": "Unknown Author", "title": "Book Four", "reviews": {} },
+    "5": { "author": "Unknown Author", "title": "Book Five", "reviews": {} },
+    "6": { "author": "Unknown Author", "title": "Book Six", "reviews": {} },
+    "7": { "author": "Unknown Author", "title": "Book Seven", "reviews": {} },
+    "8": { "author": "Unknown Author", "title": "Book Eight", "reviews": {} },
+    "9": { "author": "Unknown Author", "title": "Book Nine", "reviews": {} },
+    "10": { "author": "Unknown Author", "title": "Book Ten", "reviews": {} }
 };
 
 public_users.get('/', function (req, res) {
@@ -59,6 +54,54 @@ public_users.get('/title/:title', function (req, res) {
 public_users.get('/review/:isbn', function (req, res) {
     const isbn = req.params.isbn;
     return res.status(200).json(books[isbn].reviews);
+});
+const axios = require('axios');
+
+// Get all books using async callback function
+public_users.get('/asyncbooks', async function (req, res) {
+    try {
+        const response = await axios.get('http://localhost:5000/');
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+});
+
+// Search by ISBN using Promises
+public_users.get('/asyncisbn/:isbn', function (req, res) {
+
+    axios.get(`http://localhost:5000/isbn/${req.params.isbn}`)
+        .then(response => {
+            return res.status(200).json(response.data);
+        })
+        .catch(error => {
+            return res.status(500).json({ message: error.message });
+        });
+
+});
+
+// Search by Author
+public_users.get('/asyncauthor/:author', async function (req, res) {
+
+    try {
+        const response = await axios.get(`http://localhost:5000/author/${req.params.author}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
+});
+
+// Search by Title
+public_users.get('/asynctitle/:title', async function (req, res) {
+
+    try {
+        const response = await axios.get(`http://localhost:5000/title/${req.params.title}`);
+        return res.status(200).json(response.data);
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+
 });
 
 module.exports.general = public_users;
